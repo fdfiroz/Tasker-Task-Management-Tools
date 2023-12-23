40b5cf4310b53525e79b9ccc150256d2bac466b6
fdfiroz <firozahamed0@gmail.com>
2023-12-23 19:35:19 +0600

    Update API base URL

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuth from "./useAuth";
 import toast from "react-hot-toast";
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+    baseURL: 'https://tasker-task-management-tools-server.vercel.app/api/v1',
     withCredentials : true,
     
   });
